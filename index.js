2a9af1ddc2f7c07f41fbcfc9a218ae5235233849
hongp.02 <69109860+hongp02@users.noreply.github.com>
2020-08-15 04:30:20 +0900

    2008150430-커밋만씌바다섯번쨰다

--- index.js
+++ index.js
@@ -1,10 +1,25 @@
 const express = require('express')
 const app = express()
 const port = 5000
+const bodyParser = require('body-parser');
+const { User } = require("./models/User");
+
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.json());
 
 app.get('/', (req, res) => {
     res.send('Hello World!')
   })
+
+app.post('/register', (req, res) => {
+  const user = new User(req.body)
+  user.save((err, userinfo) => {
+    if(err) return res.json({ success: false, err})
+    return res.status(200).json({
+      success: true
+    })
+  })
+})
   
   app.listen(port, () => {
     console.log(`Example app listening at http://localhost:${port}`)
